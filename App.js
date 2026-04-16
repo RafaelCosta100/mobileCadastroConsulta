c9e524a1023639f23c6f43a12827514376ee23b5
RafaelCosta100 <rafa_sc@hotmail.com>
2026-04-15 22:01:39 -0300

    commit inicial

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CadastroScreen from './screens/CadastroScreen';
+import ConsultaScreen from './screens/ConsultaScreen';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen} options={{title:'Pagina Inicial'}}/>
+        <Stack.Screen name="Cadastro" component={CadastroScreen} />
+        <Stack.Screen name="Consulta" component={ConsultaScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
